51f45e514faa2981469e6a31e0a4957ee26a1d65
ov-git <virtaoskari@gmail.com>
2023-03-30 08:04:13 +0300

    api endpoint + client init

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+      <h1 className="text-6xl text-orange-700">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
